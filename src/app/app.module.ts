34f57831385feabcbe6b838ff8aa9d8dcff513ff
SriKavyaVanama <153215717+SriKavyaVanama@users.noreply.github.com>
2024-01-04 11:18:47 -0600

    login edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LoginModule } from './login/login.module';
-import { LoginRoutingModule } from './login/login-routing.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +10,7 @@ import { LoginRoutingModule } from './login/login-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, ReactiveFormsModule, FormsModule, LoginModule, LoginRoutingModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
